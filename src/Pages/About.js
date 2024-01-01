a4e40b98e0d924aae0e299fffc60653c06064b7e
Manmeet567 <74705756+Manmeet567@users.noreply.github.com>
2024-01-01 12:27:35 +0530

    adding source files

---
+++ src/Pages/About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Main from '../components/ForAbout/Main';
+
+const AboutUs = () => {
+    return (
+       <Main/>
+    );
+};
+
+export default AboutUs;
---
+++ src/Pages/Contact.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Main from '../components/ForContact/Main';
+
+const Contact = () => {
+    return (
+        <Main/>
+    );
+};
+
+export default Contact;
